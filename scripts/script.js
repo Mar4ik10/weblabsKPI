e04c104706ebd98e9d1d57ac2c3d4ffd2fa6a597
Mark Semeniuk <markflint2608@gmail.com>
2024-03-17 20:43:50 +0100

    Add reports to lab2

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,9 @@ let lab1 = {
     <b>Тема:</b> СТРУКТУРА HTML-ДОКУМЕНТА.ВИБІР ПРЕДМЕТНОЇ ГАЛУЗІ. РОБОТА З ПОСИЛАННЯМИ, ТАБЛИЦЯМИ, ЗОБРАЖЕННЯМИ, СПИСКАМИ.
     
     <b>Мета:</b> придбати практичні навички роботи з HTML-документом, таблицями, зображеннями, посиланнями, списками, формами. 
-    Створити шаблон звітного HTML-документом для відображення результатів роботи всіх лабораторних робіт. HTML-ДОКУМЕНТІ.`,
+    Створити шаблон звітного HTML-документом для відображення результатів роботи всіх лабораторних робіт. HTML-ДОКУМЕНТІ.
+    
+    <b>Сайт:</b> https://github.com/Mar4ik10/WebCoworking`,
     Structure: {
         Table: `
         <b>Table:</b>
@@ -111,19 +113,182 @@ let lab1 = {
 }
 
 
+let lab2 = {
+  Theme: `
+  <b>Тема</b>: КАСКАДНІ ТАБЛИЦІ СТИЛІВ. СЕЛЕКТОРИ .ІДЕНТИФІКАТОРИ. СТИЛЬОВЕ
+  ОФОРМЛЕННЯ ТЕКСТОВИХ ЕЛЕМЕНТІВ В HTML-ДОКУМЕНТАХ.
+
+  <b>Мета</b>: придбати практичні навички роботи з селекторами, ідентифікаторами,
+  списками, різноманітними властивостями кольору і фону, зовнішними та
+  внутрішними відступами, плаваючими елементами, оформленням текстових елементів
+  
+  <b>Сайт:</b> https://github.com/Mar4ik10/WebCoworking
+  `,
+  Opys: `<h1>Способи підключення:</h1>
+        
+  <h4>Вкладений CSS:</h4>
+  <p>  
+  Використовується тег &ltstyle&gt в розділі HTML-документу для включення CSS-коду безпосередньо в сторінку.
+  </p>
+   <h4>Зовнішній файл CSS:</h4>
+   <p>
+  CSS-стилі зберігаються в окремому файлі з розширенням css, а потім підключаються до HTML-сторінки за допомогою тегу &ltlink&gt.
+</p>
+   <h4>Внутрішній CSS:</h4>
+   <p>
+   Стилі включаються безпосередньо в теги HTML-елементів за допомогою атрибуту style.</p>
+  </p>`,
+  Selectors: {
+    Tags: `<h1>Селектори тегів</h1>
+    <p>
+
+
+      html {
+        scroll-behavior: smooth;
+    }
+    
+    header nav {
+        display: flex;
+        justify-content: space-between;
+        padding: 10px 20px;
+        background-color: #E5E6BA;
+    }
+    <img src="images/tags.png" alt="" width="70%">
+
+    section#Prices h1 {
+      text-align: center;
+      margin: 40px 0;
+      font-size: 70px;
+      letter-spacing: 8px;
+  }
+
+  section#Time table {
+    margin: 30px auto 0;
+    padding-bottom: 30px;
+    width: 80%;
+}`,
+    Classes: `
+    <h1>Селектори класів</h1>
+    <p>.contact-row {
+      display: flex;
+      justify-content: space-around;
+      padding-bottom: 50px;
+  }
+  
+  .contact-place {
+      display: flex;
+      border: 1px solid;
+      padding-right: 10px;
+  }
+
+  .circle {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: #F2AF4C;
+    position: relative;
+    margin: 20px 0;
+}</p>
+<img src="images/circle.png" alt="">`,
+    Ids: `
+<h1>Селектори ідентифікаторів</h1>
+<p>section#Service {
+  display: flex;
+  align-items: center;
+}
+
+<img src="images/service.png" alt="" width="40%">
+
+
+section#Time {
+background-color: #FFD2B9;
+text-align: center;
+overflow-x: auto;
+}
+</p>`,
+    Others: `
+<h1>Інші</h1>
+<p>nav .nav-buttons {
+display: flex;
+align-items: center;
+}
+
+nav .nav-buttons ul {
+display: flex;
+list-style: none;
+}
+
+nav .nav-buttons ul li > a {
+text-decoration: none;
+color: black;
+font-weight: 700;
+font-size: 1.5rem;
+margin: 0 10px;
+}</p>`
+  },
+  CSS: `
+  <h1>Шрифти</h1>
+          <p>
+          .price-card h2 {
+            font-family: "Roboto serif";
+            font-size: 35px;
+        }
+      </p>
+
+      <h1>Фон</h1>
+      <p>section#Time {
+        background-color: #FFD2B9;
+        text-align: center;
+        overflow-x: auto;
+    }</p>
+
+    <h1>Контури</h1>
+    <p>section#Time table th, td {
+      border: 1px solid black;
+      font-size: 24px;
+  }</p>
+
+  <h1>Для таблиць</h1>
+  <p>section#Time table {
+    margin: 30px auto 0;
+    padding-bottom: 30px;
+    width: 80%;
+}
+
+section#Time table th {
+    background-color: #D9D9D9;
+}
+
+section#Time table th, td {
+    border: 1px solid black;
+    font-size: 24px;
+}</p>
+  `,
+  Conclusion: `
+  Під час виконання лабораторної роботи ми успішно ознайомилися з різноманітними
+   можливостями CSS для стилізації веб-сторінок. Ми змогли ефективно використовувати 
+   різні типи селекторів для вибору елементів, задавати їм стилі, змінювати їх 
+   властивості, такі як колір, фон, відступи тощо. Це дає нам можливість створювати 
+   стильні та естетично приємні веб-сайти, що відповідають сучасним тенденціям 
+   веб-дизайну.
+  `
+}
+
+
 let currentelement;
 let lastelement;
 
 let containing = document.querySelector("section pre");
 
 function displaylabbuttons(buttonid) {
-    if (lastelement){
-        lastelement = currentelement;
-        lastelement.style.display = "none";
-    }
-    
-    currentelement = document.getElementById(buttonid);
-    currentelement.style.display = "flex";
+  if (lastelement) {
+    lastelement.style.display = "none";
+  }
+
+  currentelement = document.getElementById(buttonid);
+  currentelement.style.display = "flex";
+
+  lastelement = currentelement;
 }
 
 
@@ -135,24 +300,57 @@ function displayinfo(button) {
 
     switch (parentelement) {
         case "aside-button-lab1":
-            switch (classelementName) {
-                case "aside-button1":
-                    containing.innerHTML = lab1.Opys;
-                    break;
-                
-                case "aside-button2":
-                    containing.innerHTML = lab1.Theme;
-                    break;
+          if (classelementName == "aside-button1") {
+            containing.innerHTML = lab1.Opys;
+          }
+          else if (classelementName == "aside-button2"){
+            containing.innerHTML = lab1.Theme;
+          }
+          else if (classelementName == "aside-button3"){
+            containing.innerHTML = lab1.Structure.Table + "\n" +lab1.Structure.Form + "\n" + lab1.Structure.Image;
 
-                case "aside-button3":
-                    containing.innerHTML = lab1.Structure.Table + "\n" +lab1.Structure.Form + "\n" + lab1.Structure.Image;
-                    break;
+          }
+          else if (classelementName == "aside-button4"){
+            containing.innerHTML = lab1.Conclusion;
+          }
+          break;
 
-                case "aside-button4":
-                    containing.innerHTML = lab1.Conclusion;
-                    break;
+          case "aside-button-lab2":
+            if (classelementName == "aside-button1") {
+              containing.innerHTML = lab2.Theme;
             }
+            else if (classelementName == "aside-button2"){
+              containing.innerHTML = lab2.Opys;
+            }
+            else if (classelementName == "aside-button3"){
+              containing.innerHTML = lab2.Selectors.Tags + "\n" +lab2.Selectors.Classes + "\n" + lab2.Selectors.Ids;
+  
+            }
+            else if (classelementName == "aside-button4"){
+              containing.innerHTML = lab2.CSS;
+            }
+            else if (classelementName == "aside-button5"){
+              containing.innerHTML = lab2.Conclusion;
+            }
+            break;
 
-        
-    }
+          case "aside-button-lab3":
+            if (classelementName == "aside-button1") {
+              containing.innerHTML = lab3.Theme;
+            }
+            else if (classelementName == "aside-button2"){
+              containing.innerHTML = lab3.Opys;
+            }
+            else if (classelementName == "aside-button3"){
+              containing.innerHTML = lab3
+  
+            }
+            else if (classelementName == "aside-button4"){
+              containing.innerHTML = lab3.CSS;
+            }
+            else if (classelementName == "aside-button5"){
+              containing.innerHTML = lab3.Conclusion;
+            }
+            break;
+            }
 }
